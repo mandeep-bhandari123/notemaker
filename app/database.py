314a82b21f52327a20ac8a090dfcac37e827177e
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-06-22 22:13:10 +0545

    Code Update

---
+++ app/database.py
@@ -0,0 +1,9 @@
+#connecting to the databse
+from sqlalchemy import create_engine 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os 
+
+load_dotenv(dotenv_path=r"C:\Users\nishc\project\notemaker\app\.env")
+SQLALCHAMY_DATABASE_URL = os.getenv("DATABASE_URL")
