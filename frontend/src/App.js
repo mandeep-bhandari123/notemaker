bb373deac15fb9a4fd393bf162d97560980e61b9
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-07-13 21:44:30 +0545

    Code Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,50 @@
-function MyButton(){
-  return(
-    <button>I'm a button</button>
-  );
-};
+// function MyButton(){
+//   return(
+//     <button>I'm a button</button>
+//   );
+// };
 
 
-function Logo(){
-  const user ={
-    logo:"/logo.png"
+// function Logo(){
+//   const user ={
+//     logo:"/logo.png"
     
-  };
-  return (
-    <img 
-    className="logo"
-    src={user.logo}
-    alt="Logo"
-    style={{width:"100px",height:"auto"}}>
+//   };
+//   return (
+//     <img 
+//     className="logo"
+//     src={user.logo}
+//     alt="Logo"
+//     style={{width:"100px",height:"auto"}}>
 
-    </img>
-  )
-}
+//     </img>
+//   )
+// }
 
-var user = "mandeep"
+// var user = "mandeep"
+// export default function MyApp(){
+//   return(
+//     <div>
+//       <Logo />
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+
+
+//     </div>
+//   )
+// }
+
+
+import logo from './logo.png';
+import './App.css';
+
+
 export default function MyApp(){
   return(
-    <div>
-      <Logo />
-      <h1>Welcome to my app</h1>
-      <MyButton />
-
-
+    <div className='app-container'>
+      <img className='logo' src={logo} alt = {logo} />
+      
     </div>
   )
 }
+
