0f53822722fee4b39efd997bd8a54469a97c5272
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-07-13 16:31:53 +0545

    Code Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,32 @@ function MyButton(){
   );
 };
 
+
+function Logo(){
+  const user ={
+    logo:"/logo.png"
+    
+  };
+  return (
+    <img 
+    className="logo"
+    src={user.logo}
+    alt="Logo"
+    style={{width:"100px",height:"auto"}}>
+
+    </img>
+  )
+}
+
+var user = "mandeep"
 export default function MyApp(){
   return(
     <div>
+      <Logo />
       <h1>Welcome to my app</h1>
       <MyButton />
-      
+
+
     </div>
   )
 }
