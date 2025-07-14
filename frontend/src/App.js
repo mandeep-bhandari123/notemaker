4d2a0444683a4f3a2e8371a878b0a4ef4e1daf32
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-07-14 17:03:55 +0545

    Code Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,3 @@
-// function MyButton(){
-//   return(
-//     <button>I'm a button</button>
-//   );
-// };
-
-
-// function Logo(){
-//   const user ={
-//     logo:"/logo.png"
-    
-//   };
-//   return (
-//     <img 
-//     className="logo"
-//     src={user.logo}
-//     alt="Logo"
-//     style={{width:"100px",height:"auto"}}>
-
-//     </img>
-//   )
-// }
-
-// var user = "mandeep"
-// export default function MyApp(){
-//   return(
-//     <div>
-//       <Logo />
-//       <h1>Welcome to my app</h1>
-//       <MyButton />
-
-
-//     </div>
-//   )
-// }
-
 
 import logo from './logo.png';
 import './App.css';
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,15 +1,20 @@
-import './Navbar.css'
 import React from 'react'
-export default function navbar(){
+import './Navbar.css'
+import logo from './logo.png'; // adjust path if needed
+
+export default function Navbar(){
     return(
         <nav className='navbar'>
-            <div className='logo'>Notemaker</div>
-            <ul className='nav-links'>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>Features</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Contact</a></li>
-            </ul>
-        </nav>
+    <img src={logo} alt="Notemaker Logo" className='navbar-logo' />
+    <div className='center-links'>
+        <ul className='nav-links'>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Features</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Contact</a></li>
+        </ul>
+    </div>
+</nav>
+
     )
 }
