222addcc7278c47ac92e797006df32af1f064fb5
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-07-14 10:55:42 +0545

    Code Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,10 @@ export default function MyApp(){
   return(
     <div className='app-container'>
       <img className='logo' src={logo} alt = {logo} />
-      
+      <div className='button-group'>
+        <button className='btn login-btn'>Login</button>
+        <button className='btn signup-btn'>Sing Up</button>
+      </div>
     </div>
   )
 }
