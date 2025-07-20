6fabcb1fac84c35a4d0a413c65114858cee21b94
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-07-20 22:04:50 +0545

    Code Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,48 @@
-
+// src/App.js
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import logo from './logo.png';
 import './App.css';
 import Navbar from './Navbar';
 
-export default function MyApp(){
-  return(
-    <div className='app-container'>
-      <Navbar />
-      <img className='logo' src={logo} alt = {logo} />
-      <div className='button-group'>
-        <button className='btn login-btn'>Login</button>
-        <button className='btn signup-btn'>Sing Up</button>
-      </div>
-    </div>
-  )
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/"      element={<Home   />} />
+      <Route path="/login"  element={<Login  />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 }
 
+function Home() {
+  const navigate = useNavigate();
+  return (
+    <div className="app-container">
+      <Navbar />
+      <img className="logo" src={logo} alt="Logo" />
+      <div className="button-group">
+        <button className="btn login-btn"  onClick={() => navigate('/login')}> Login </button>
+        <button className="btn signup-btn" onClick={() => navigate('/signup')}> Sign Up </button>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <h2>Login</h2>
+    </div>
+  );
+}
+
+function Signup() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <h2>Signup Page</h2>
+    </div>
+  );
+}
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import './Navbar.css'
-import logo from './logo.png'; // adjust path if needed
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from './logo.png'; // make sure this path is correct
 
 export default function Navbar(){
     return(
         <nav className='navbar'>
-    <img src={logo} alt="Notemaker Logo" className='navbar-logo' />
-    <div className='center-links'>
-        <ul className='nav-links'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Features</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>Contact</a></li>
-        </ul>
-    </div>
-</nav>
-
+            <img src={logo} alt="Notemaker Logo" className='navbar-logo' />
+            <div className='center-links'>
+                <ul className='nav-links'>
+                    <li><Link to="/">Home</Link></li>
+                    <li><a href='#'>Features</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Contact</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
