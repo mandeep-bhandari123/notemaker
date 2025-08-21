56e97b40770b90feb6481420497702ac24ddb190
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-08-21 16:48:40 +0545

    Code Update

--- app/oauth2.py
+++ app/oauth2.py
@@ -15,7 +15,7 @@ oauth2_scheme  =OAuth2PasswordBearer(tokenUrl='login')
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
-ACCESS_TOKEN_EXPIRE_TIME = 2
+ACCESS_TOKEN_EXPIRE_TIME = 60
 
 def create_access_token(data: dict):
     to_encode = data.copy()
--- app/routers/summarize.py
+++ app/routers/summarize.py
@@ -1,6 +1,7 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain.schema.output_parser import StrOutputParser
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain.prompts import ChatPromptTemplate 
 from .. import models , schemas , utils ,oauth2
@@ -18,23 +19,6 @@ router = APIRouter(
 load_dotenv()  
 text =""
 
-import re
-
-def clean_text(markdown_text: str) -> str:
-    # Remove Markdown bold/italic markers
-    text = re.sub(r"\*\*(.*?)\*\*", r"\1", markdown_text)  # **bold**
-    text = re.sub(r"\*(.*?)\*", r"\1", text)               # *italic*
-    
-    # Remove headings (###, ##, #)
-    text = re.sub(r"#+\s*", "", text)
-    
-    # Replace multiple newlines with a single space
-    text = re.sub(r"\n+", " ", text)
-    
-    # Remove extra spaces
-    text = re.sub(r"\s{2,}", " ", text).strip()
-    
-    return text
 
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")  
@@ -44,13 +28,15 @@ model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 @router.post('')
 def summarize (text:str, db:session=Depends(get_db),current_user: schemas.TokenData = Depends(oauth2.get_current_user)):
-    template =[('system', "You are a helpful chat bot whats gices detailed sumarry and notes to the provided text."), ('human', 'Please give me a detailed notes of this {text} ')]
-    prompt_template = ChatPromptTemplate.from_messages(template)
-    prompt = prompt_template.invoke({"text":text})
-    result =model.invoke(prompt)
-    output = result.content
-    output = clean_text(output)
-    return output
+    prompt_template =ChatPromptTemplate.from_messages([('system', "You are a helpful chat bot whats gices detailed sumarry and notes to the provided text."), ('human', 'Please give me a detailed notes of this {text} ')])
+    
+    
+    
+    
+    chain = prompt_template | model | StrOutputParser()
+    
+    result =chain.invoke({'text':text})
+    
      
 
 
